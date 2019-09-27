84f6870f759bcf6d531cd9523548ab354f637f7a
bubbleztoo <neil.ray@colorado.edu>
2019-09-27 11:45:42 -0600

    Created lab3_script.sh

--- lab3_script.sh
+++ lab3_script.sh
@@ -4,3 +4,14 @@
 
 #Problem 1 Code:
 #Make sure to document how you are solving each problem!
+echo "Enter a file name:"
+read fileName
+echo "Enter a regex:"
+read regEx
+grep regEx fileName
+grep -c ??? "regex_practice.txt"
+grep -c ??? "regex_practice.txt"
+grep ??? "regex_practice.txt"
+grep ??? "regex_practice.txt" >> "email_results.txt"
+git add "email_results.txt"
+git commit -m "email_results.txt"
